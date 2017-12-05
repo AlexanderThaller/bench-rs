95e500d705472e314afdad0ab6e4dbe486637eb0
Alexander Thaller <alexander.thaller@trivago.com>
2017-12-05 17:39:06 +0100

    Fixed problem that program would only be run once and then never again.

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,20 @@ fn main() {
     args.next();
 
     let command_name = args.next().unwrap();
-    let mut command = Command::new(command_name);
-    command.args(args);
+    let command_args: Vec<_> = args.map(|x| x.into()).collect();
 
-    Criterion::default().bench_function(format!("{:?}", command).as_str(), |b| {
-        b.iter(|| command.output())
-    });
+    Criterion::default().sample_size(10).bench_function(
+        format!("{} {}", command_name, command_args.join(" ")).as_str(),
+        |b| {
+            b.iter(|| {
+                create_command(command_name.as_str(), &command_args).output()
+            })
+        },
+    );
+}
+
+fn create_command(name: &str, args: &[String]) -> Command {
+    let mut command = Command::new(name);
+    command.args(args);
+    command
 }
